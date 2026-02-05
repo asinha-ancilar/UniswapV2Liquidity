0707fe72b543f949954d6f98bbf223d3b623280e
Ancilar <ancilar@Ancilars-Laptop.local>
2026-02-05 18:05:30 +0530

    adding deployment script and ERC20 Token contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,9 @@ require("@nomicfoundation/hardhat-verify");
 module.exports = {
   solidity: "0.8.33",
   networks: {
+    localhost: {
+        url: `http://127.0.0.1:8545/`
+    },
     sepolia: {
        url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
        accounts: [process.env.PRIVATE_KEY],
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,27 @@
 const logger = require('../utils/logger');
+const { ethers } = require("hardhat");
 
-async function main(){}
+async function main(){
+  const Token = await ethers.getContractFactory("Token");
+
+  const token0 = await Token.deploy("Token0","TK0");
+  const token1 = await Token.deploy("Token1","TK1");
+
+  await token0.waitForDeployment();
+  await token1.waitForDeployment();
+
+  const Pool = await ethers.deployContract('UniswapV2Liquidity',[
+    await token0.getAddress(),
+    await token1.getAddress()
+  ])
+
+  await Pool.waitForDeployment();
+
+  logger.info('Pool address: ', await Pool.getAddress());
+  logger.info('Token-0 address: ',await token0.getAddress());
+  logger.info('Token-1 address: ',await token1.getAddress());
+
+}
 
 main().catch((error) => {
   logger.error(error);
